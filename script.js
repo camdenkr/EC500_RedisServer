81ba04dd73a6fac4c728f68114e73edcaf3f5d4b
Camden Kronhaus <tweet3r@hotmail.com>
2020-10-04 18:49:49 +0000

    Force user sign in first

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 let console_output = ""
 let salt = 23541 //use salt for requests
+var hash_pass
+var signed_in = 0
 function send_function() {
-  
-    //if command is empty, return immedieately (aka do nothing)
+     //if command is empty, return immedieately (aka do nothing)
     let usr_input = document.getElementById("cmdprompt").value
     if(usr_input == "")
       return
      var split_input = usr_input.split(" ");
      let input_len = split_input.length;
-     
-
+    
+    //make sure user signs in first
     //check login command, only login if that's the case
     if(split_input[0] == "PASSWORD")
     {
-      var hash_pass = (md5(salt + split_input[1]))
+      signed_in = 1
+      hash_pass = (md5(salt + split_input[1]))
+      clear_input()
+      return
     }
-    else //if a valid REDIS command, assume message is legitimate, take the rest of the command as the message, check for errors in request
+    else if(signed_in == 0)
     {
+      document.getElementById('console').innerHTML = "Error: Please sign in with PASSWORD first\n"
+      clear_input()
+      return
+    }
+    
+   
+     
+
+    
+    //else //if a valid REDIS command, assume message is legitimate, take the rest of the command as the message, check for errors in request
+    //{
       // var message = ""
       // for (let i = 0; i < input_len; i++) { 
       //   message = message + split_input[i]
@@ -24,15 +39,16 @@ function send_function() {
       //   if(i != input_len - 1)
       //     message = message + " "
      // }
-    }
-    console_output = console_output + document.getElementById("cmdprompt").value + "\n" //sets the whole console output to history plus the newly gathered data
+    //}
+    console_output = "$ " + console_output + document.getElementById("cmdprompt").value + "\n" //sets the whole console output to history plus the newly gathered data
     document.getElementById('console').innerHTML = console_output; //sets value inside console to console_output
-    document.getElementById('cmdprompt').value = "" //clears input for better UX
+    clear_input()
 
   //create command to send to URL, whitespaces are automatically parsed to URL format when sent
   let fetch_command = "https://agile.bu.edu/ec500_scripts/redis.php?salt=" + String(salt) + "&hash=" + String(hash_pass) + "&message=" + String(usr_input);
+  console.log(fetch_command)
     
-    fetch("https://agile.bu.edu/ec500_scripts/redis.php?salt=23541&hash=10851ed441e8c524fb23abd321b20248&message=SET foo432 a")
+    fetch(fetch_command)
   .then(function (response) {
     return response.json();
   })
@@ -44,4 +60,8 @@ function send_function() {
   });
   }
 
-
+//clears input for better UX
+function clear_input()
+{
+  document.getElementById('cmdprompt').value = "" 
+}
